f82f81c3e6f254738326a849b0aba7aa78f6fb6d
margro26 <bernd.gs63@gmail.com>
2022-04-16 22:49:34 +0200

    commit

--- Polymorphie/Markt/src/Zugriff.java
+++ Polymorphie/Markt/src/Zugriff.java
@@ -2,10 +2,14 @@ public class Zugriff{
     public static void main(String[] args) {
         Milchpackung milch = new Milchpackung(1.98, 1, 8, "22.04.2022",
                 true, "Mandelmilch");
+        milch.gebeMilchpackungAus();
         Schokoladentafel schoko = new Schokoladentafel(1.09, 1, 15, "15.4.2024",
                 38.0, "Oreo-Schokolade");
+        schoko.gebeSchokoladentafelAus();
         Möbelstück moeb = new Möbelstück(35.00, 1, 400, 50, 100, "Sofa");
+        moeb.gebeMöbelstückAus();
         Kleidungsstück kleid = new Kleidungsstück(9.99, 1, 38, "T-Shirt", "rot",
                 "Baumwolle");
+        kleid.gebeKleidungsstückAus();
     }
 }
