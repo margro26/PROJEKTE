fdd5d4adce7f697720f2387aace921c55be3e0f8
margro26 <bernd.gs63@gmail.com>
2022-04-24 21:48:56 +0200

    commit

--- Polymorphie/Markt/src/Milchpackung.java
+++ Polymorphie/Markt/src/Milchpackung.java
@@ -1,7 +1,6 @@
 public class Milchpackung extends Lebensmittel{
     protected boolean Hocherhitzt;
     protected String Typ;
-    protected String fragehocherhitzt;
 
     public Milchpackung(double preis, int anzahl, int maxLagerTemperatur, String verfallDatum, boolean hocherhitzt, String typ){
         super(preis, anzahl, maxLagerTemperatur, verfallDatum);
@@ -9,30 +8,17 @@ public class Milchpackung extends Lebensmittel{
         Typ = typ;
     }
 
-    public void setHocherhitzt(boolean hocherhitzt) {
-        Hocherhitzt = hocherhitzt;
-    }
-    public boolean  isHocherhitzt() {
+    public boolean isHocherhitzt() {
         return Hocherhitzt;
     }
-    public void setTyp(String typ) {
-        Typ = typ;
-    }
+
     public String getTyp() {
         return Typ;
-    }
-
-    boolean h = Hocherhitzt;
-
-    if (h == true) {
-        fragehocherhitzt = "hocherhitzt";
-    } else {
-        fragehocherhitzt = "nicht hocherhitzt";
     }
 
     public void gebeMilchpackungAus(){
         System.out.println(getAnzahl() + " " + getTyp() + " kostet " + getPreis() + "€. Bitte lagern Sie die " +
                 getTyp() + " bei höchstens " + getMaxLagerTemperatur() + "°C und verzehren Sie diese bis spätestens " +
-                getVerfallDatum() + ". Die " + getTyp() + " ist " + fragehocherhitzt + ".");
+                getVerfallDatum() + ". Die " + getTyp() + " ist " + isHocherhitzt() + ".");
     }
 }
